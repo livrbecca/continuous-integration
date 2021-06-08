bbcbbf6caafdee3e607164b65e7e28c201191d8e
livrbecca <ollywalker_1998@outlook.com>
2021-06-08 12:57:21 +0100

    passed prettier checks

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -7,15 +7,16 @@
  * @param {string[]} stringArr - array of strings
  * @returns {string} - the shortest string
  */
-function findShortestString(
-stringArr
-)
-{ const shortestString = stringArr[0]
-    for (let str of stringArr) {
-if (str.length < shortestString.length) { shortestString = str
-    }}  return shortestString.length }
+function findShortestString(stringArr) {
+  let shortestString = stringArr[0];
+  for (let str of stringArr) {
+    if (str.length < shortestString.length) {
+      shortestString = str;
+    }
+  }
+  return shortestString.length;
+}
 
 console.log(findShortestString(["it", "is", "a", "nice", "day"]), "a");
 console.log(findShortestString(["why", "hello", "to", "you"]), "to");
-console.log(findShortestString(["brave", "dance"]), "brave")
-
+console.log(findShortestString(["brave", "dance"]), "brave");
