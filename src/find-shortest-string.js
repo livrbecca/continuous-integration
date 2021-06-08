13eccff4b7c7a60d68ed653f2b0a1ae427e008c0
livrbecca <ollywalker_1998@outlook.com>
2021-06-08 13:00:36 +0100

    lint and prettier checks passed

--- src/find-shortest-string.js
+++ src/find-shortest-string.js
@@ -1,8 +1,8 @@
 "use strict";
 
 /**
- * Find the shortest string in an array.
- * In the case of a tie, returns the earlier string in the array.
+ * Find the shortest string in an array
+ * In the case of a tie, returns the earlier string in the array
  *
  * @param {string[]} stringArr - array of strings
  * @returns {string} - the shortest string
